b6557a2f7e87a488c4992f42c1f3a4d93f4c1e1d
samridhav14 <100757567+samridhav14@users.noreply.github.com>
2022-10-08 15:41:40 +0530

    Updated home_page.dart

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -14,6 +14,8 @@ import 'pages/page_12.dart';
 import 'pages/page_13.dart';
 import 'pages/page_14.dart';
 import 'pages/page_15.dart';
+import 'pages/page_17.dart';
+
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -42,6 +44,7 @@ class HomePage extends StatelessWidget {
           ),
           PageButton(pageNumber: 14, page: Page14()),
           PageButton(pageNumber: 15, page: Page15()),
+          PageButton(pageNumber: 17, page: Page17()),
 
           /// TODO:
           /// Want to add more page buttons?
