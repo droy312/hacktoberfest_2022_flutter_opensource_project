dec316274243fed2c80d7af6911debc6595c4d8c
sanskriti242 <97687295+sanskriti242@users.noreply.github.com>
2022-10-14 18:08:30 +0530

    Updated page_18.dart
    Beautified page

--- lib/pages/page_18.dart
+++ lib/pages/page_18.dart
@@ -1,13 +1,48 @@
 import 'package:flutter/material.dart';
-
 // TODO: Beautify this page according to your imagination
 class Page18 extends StatelessWidget {
   const Page18({super.key});
-
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Center(child: Text('Page 18')),
+    return Scaffold(
+      backgroundColor: Colors.yellow,
+      appBar: AppBar(
+        leading: const Icon(Icons.arrow_back),
+        title: const Text('Page 18'),
+        centerTitle: true,
+        elevation: 0,
+        backgroundColor: Colors.deepPurple[600],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Konnichiwa',
+              style: TextStyle(
+                  fontSize: MediaQuery.of(context).size.width * 0.15,
+                  fontWeight: FontWeight.light,
+                  color: Colors.pink[600]),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Image.network(
+                '',
+              ),
+            ),
+            const Text(
+              'page 18',
+              style: TextStyle(
+                color: Colors.aqua,
+                fontWeight: FontWeight.light,
+                fontSize: 50,
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
