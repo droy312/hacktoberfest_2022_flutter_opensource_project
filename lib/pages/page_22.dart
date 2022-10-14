e05ff502ca5751bde943e8d9bcc3308d189c796d
EvilShadow1518 <115689489+EvilShadow1518@users.noreply.github.com>
2022-10-14 19:03:08 +0530

    created page 22
    created page 22

---
+++ lib/pages/page_22.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class Page22 extends StatelessWidget {
+  const Page22({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Icon(Icons.arrow_back_ios),
+        title: const Text('Page 22'),
+        centerTitle: true,
+        elevation: 0,
+        backgroundColor: Colors.purple[600],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Hi There!!',
+              style: TextStyle(
+                  fontSize: MediaQuery.of(context).size.width * 0.15,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.purple[600]),
+            ),
+            Stack(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Image.network(
+                    'https://media.istockphoto.com/photos/hello-and-welcome-written-white-lightbox-sitting-on-blue-background-picture-id1371547852?b=1&k=20&m=1371547852&s=170667a&w=0&h=vRGvMO4a7KKYdTQ9Ln1UVdlg5C0POExw73jGsotVgzA=',
+                  ),
+                ),
+                const Positioned(
+                  bottom: 30,
+                  right: 90,
+                  child: Text(
+                    'To Page 22',
+                    style: TextStyle(
+                        color: Colors.amberAccent,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 40),
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
