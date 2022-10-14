b984b76af6b68c280aa97015bbae0d0a1566aa77
Akhilesh Kumar Singh <100690788+Akhileshkumarsingh21221@users.noreply.github.com>
2022-10-14 19:00:45 +0530

    Created page_22.dart

---
+++ lib/pages/page_22.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+// TODO: Beautify this page according to your imagination
+class Page22 extends StatelessWidget {
+  const Page22({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('Page 22')),
+    );
+  }
+}
