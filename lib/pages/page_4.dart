782e404083ffd0fe1ff313812e9b16b3c49a6205
droy312 <dhritiman.roy312@gmail.com>
2022-10-15 12:53:53 +0530

    feat(page_4): Added page 4 and created an UI

--- lib/pages/page_4.dart
+++ lib/pages/page_4.dart
@@ -6,8 +6,85 @@ class Page4 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('Page 4')),
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              const Text(
+                "Anna williams",
+              ),
+              const Text(
+                "Calling…",
+                style: TextStyle(color: Colors.white60),
+              ),
+              const Spacer(),
+              Wrap(
+                alignment: WrapAlignment.spaceBetween,
+                children: const [
+                  DialButton(
+                    iconSrc: "assets/icons/Icon Mic.svg",
+                    text: "Audio",
+                  ),
+                  DialButton(
+                    iconSrc: "assets/icons/Icon Volume.svg",
+                    text: "Microphone",
+                  ),
+                  DialButton(
+                    iconSrc: "assets/icons/Icon Video.svg",
+                    text: "Video",
+                  ),
+                  DialButton(
+                    iconSrc: "assets/icons/Icon Message.svg",
+                    text: "Message",
+                  ),
+                  DialButton(
+                    iconSrc: "assets/icons/Icon User.svg",
+                    text: "Add contact",
+                  ),
+                  DialButton(
+                    iconSrc: "assets/icons/Icon Voicemail.svg",
+                    text: "Voice mail",
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class DialButton extends StatelessWidget {
+  const DialButton({
+    Key? key,
+    required this.iconSrc,
+    required this.text,
+  }) : super(key: key);
+
+  final String iconSrc, text;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 120,
+      child: TextButton(
+        onPressed: () {},
+        child: Column(
+          children: [
+            const SizedBox(height: 5),
+            Text(
+              text,
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 13,
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
