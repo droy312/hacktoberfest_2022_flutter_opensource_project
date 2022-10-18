882d4a95f7c5081aec3531dd0d46edefc56718e2
pandeyji-mayank <97236747+pandeyji-mayank@users.noreply.github.com>
2022-10-18 19:33:59 +0530

    Created page_29.dart

---
+++ lib/pages/page_29.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+// TODO: Beautify this page according to your imagination
+class Page29 extends StatelessWidget {
+  const Page29({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('Page 29')),
+    );
+  }
+}
