429aeb184e6212f9531b51289f89177ead7a47f0
Pawan Acharya <pawanacharya@Pawans-MacBook-Pro.local>
2022-10-20 09:00:19 +0545

    changed to grid layout

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -32,59 +32,68 @@ import 'pages/page_29.dart';
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
+  final pages = const[
+    PageButton(pageNumber: 1, page: Page1()),
+    PageButton(pageNumber: 2, page: Page2()),
+    PageButton(pageNumber: 3, page: Page3()),
+    PageButton(pageNumber: 4, page: Page4()),
+    PageButton(pageNumber: 5, page: Page5()),
+    PageButton(pageNumber: 6, page: Page6()),
+    PageButton(pageNumber: 7, page: Page7()),
+    PageButton(pageNumber: 8, page: Page8()),
+    PageButton(pageNumber: 9, page: Page9()),
+    PageButton(pageNumber: 10, page: Page10()),
+    PageButton(pageNumber: 11, page: Page11()),
+    PageButton(pageNumber: 12, page: Page12()),
+    Hero(
+      tag: 'page_13_transition_key',
+      child: PageButton(pageNumber: 13, page: Page13()),
+    ),
+    PageButton(pageNumber: 14, page: Page14()),
+    PageButton(pageNumber: 15, page: Page15()),
+    PageButton(pageNumber: 16, page: Page16()),
+    PageButton(pageNumber: 17, page: Page17()),
+    PageButton(pageNumber: 18, page: Page18()),
+    PageButton(pageNumber: 19, page: Page19()),
+    PageButton(pageNumber: 20, page: Page20()),
+    PageButton(pageNumber: 21, page: Page21()),
+    PageButton(pageNumber: 22, page: Page22()),
+    PageButton(pageNumber: 23, page: Page23()),
+    PageButton(pageNumber: 24, page: Page24()),
+    PageButton(pageNumber: 25, page: Page25()),
+    PageButton(pageNumber: 26, page: Page26()),
+    PageButton(pageNumber: 27, page: Page27()),
+    PageButton(pageNumber: 28, page: Page28()),
+    PageButton(pageNumber: 29, page: Page29()),
+
+    /// TODO:
+    /// Want to add more page buttons?
+    ///
+    /// Add the below code snippet
+    /// Add the proper page number
+    /// Create a new page in the pages folder
+    /// Add the newly created Page widget in the page named parameter
+    ///
+    /// Code snippet
+    /// ```dart
+    /// PageButton(pageNumber: /*proper page number*/, page: /*newly created page number*/),
+    /// ```
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        children: const [
-          SizedBox(height: 100),
-          PageButton(pageNumber: 1, page: Page1()),
-          PageButton(pageNumber: 2, page: Page2()),
-          PageButton(pageNumber: 3, page: Page3()),
-          PageButton(pageNumber: 4, page: Page4()),
-          PageButton(pageNumber: 5, page: Page5()),
-          PageButton(pageNumber: 6, page: Page6()),
-          PageButton(pageNumber: 7, page: Page7()),
-          PageButton(pageNumber: 8, page: Page8()),
-          PageButton(pageNumber: 9, page: Page9()),
-          PageButton(pageNumber: 10, page: Page10()),
-          PageButton(pageNumber: 11, page: Page11()),
-          PageButton(pageNumber: 12, page: Page12()),
-          Hero(
-            tag: 'page_13_transition_key',
-            child: PageButton(pageNumber: 13, page: Page13()),
-          ),
-          PageButton(pageNumber: 14, page: Page14()),
-          PageButton(pageNumber: 15, page: Page15()),
-          PageButton(pageNumber: 16, page: Page16()),
-          PageButton(pageNumber: 17, page: Page17()),
-          PageButton(pageNumber: 18, page: Page18()),
-          PageButton(pageNumber: 19, page: Page19()),
-          PageButton(pageNumber: 20, page: Page20()),
-          PageButton(pageNumber: 21, page: Page21()),
-          PageButton(pageNumber: 22, page: Page22()),
-          PageButton(pageNumber: 23, page: Page23()),
-          PageButton(pageNumber: 24, page: Page24()),
-          PageButton(pageNumber: 25, page: Page25()),
-          PageButton(pageNumber: 26, page: Page26()),
-          PageButton(pageNumber: 27, page: Page27()),
-          PageButton(pageNumber: 28, page: Page28()),
-          PageButton(pageNumber: 29, page: Page29()),
-          
-          /// TODO:
-          /// Want to add more page buttons?
-          ///
-          /// Add the below code snippet
-          /// Add the proper page number
-          /// Create a new page in the pages folder
-          /// Add the newly created Page widget in the page named parameter
-          ///
-          /// Code snippet
-          /// ```dart
-          /// PageButton(pageNumber: /*proper page number*/, page: /*newly created page number*/),
-          /// ```
-        ],
-      ),
+        body: GridView.builder(
+          itemCount: pages.length,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3,
+              crossAxisSpacing: 8,
+              mainAxisSpacing: 8,
+              childAspectRatio: (2 / 1),
+            ),
+            itemBuilder: (context, index)  {
+            return pages[index];
+        })
     );
   }
 }
@@ -92,6 +101,7 @@ class HomePage extends StatelessWidget {
 class PageButton extends StatelessWidget {
   final int pageNumber;
   final Widget page;
+
   const PageButton({
     super.key,
     required this.pageNumber,
