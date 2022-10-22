62cab1be2f8a65c45092f5148973cca9f8711709
dawnCoder1518 <116420641+dawnCoder1518@users.noreply.github.com>
2022-10-22 11:04:35 +0530

    created page_31.dart file

---
+++ lib/pages/page_31.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+// TODO: Beautify this page according to your imagination
+class Page31 extends StatelessWidget {
+  const Page31({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('Welcome to Page 31')),
+    );
+  }
+}
