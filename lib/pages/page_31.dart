43dd13c1c964f903c62d923df72efb770786fed2
dawnCoder1518 <116420641+dawnCoder1518@users.noreply.github.com>
2022-10-22 11:29:29 +0530

    beautified page_31.dart

--- lib/pages/page_31.dart
+++ lib/pages/page_31.dart
@@ -1,13 +1,53 @@
 import 'package:flutter/material.dart';
 
-// TODO: Beautify this page according to your imagination
 class Page31 extends StatelessWidget {
   const Page31({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('Welcome to Page 31')),
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Icon(Icons.arrow_back_ios),
+        title: const Text('Page 31'),
+        centerTitle: true,
+        elevation: 0,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Hi!!',
+              style: TextStyle(
+                  fontSize: MediaQuery.of(context).size.width * 0.15,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.red[600]),
+            ),
+            Stack(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Image.network(
+                    'https://images.unsplash.com/photo-1666059369018-bb3e93e413c0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80',
+                  ),
+                ),
+                const Positioned(
+                  bottom: 30,
+                  right: 90,
+                  child: Text(
+                    'To Page 31',
+                    style: TextStyle(
+                        color: Colors.blue,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 40),
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
