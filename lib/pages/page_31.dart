389a4de4819742e6e3e595550f91aff43f2e323e
dawnCoder1518 <116420641+dawnCoder1518@users.noreply.github.com>
2022-10-22 11:51:21 +0530

    beautified page_31.dart

--- lib/pages/page_31.dart
+++ lib/pages/page_31.dart
@@ -6,8 +6,45 @@ class Page31 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('Welcome to Page 31')),
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text('Welcome to Page 31'),
+          elevation: 0,
+          backgroundColor: Colors.black26,
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              PhysicalModel(
+                color: Colors.white,
+                elevation: 50,
+                shadowColor: Colors.white,
+                child: Container(
+                  alignment: Alignment.center,
+                  height: 200,
+                  width: 200,
+                  color: Colors.black87,
+                  child: const Center(
+                    child: Padding(
+                      padding: EdgeInsets.all(20.0),
+                      child: Text(
+                        'Hello and Welcome to Page 31!!',
+                        style: TextStyle(
+                          fontSize: 30,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
