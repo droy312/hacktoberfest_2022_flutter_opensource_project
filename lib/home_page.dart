f41977cf7d8fc9980190d7497bacf117a036a798
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2023-09-10 23:56:26 +0200

    The libraries that will be used

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -17,3 +17,29 @@ import 'pages/page_14.dart';
 import 'pages/page_15.dart';
 import 'pages/page_16.dart';
 import 'pages/page_17.dart';
+import 'pages/page_18.dart';
+import 'pages/page_19/page_19.dart';
+import 'pages/page_20.dart';
+import 'pages/page_21.dart';
+import 'pages/page_22.dart';
+import 'pages/page_23.dart';
+import 'pages/page_24.dart';
+import 'pages/page_25.dart';
+import 'pages/page_26.dart';
+import 'pages/page_27/page_27.dart';
+import 'pages/page_28.dart';
+import 'pages/page_29.dart';
+import 'pages/page_30.dart';
+import 'pages/page_31.dart';
+import 'pages/page_32.dart';
+import 'pages/page_33.dart';
+import 'pages/page_34.dart';
+import 'pages/page_35.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}); // Corrected the constructor syntax
+
+  final pages = <Widget>[
+    const SizedBox(height: 100),
+    const SizedBox(height: 100),
+    const SizedBox(height: 100),
